598be65b05c86ff19ad17939c74da72dd10973b4
Nicolas Huber <nicolas.huber2@uzh.ch>
2024-03-14 15:51:28 +1100

    Exercises week 4

---
+++ 43031/Workshop4/calculator.py
@@ -0,0 +1,16 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+
+def square_root(x):
+    return x**0.5
+
+def is_even(x):
+    if x%2 == 0:
+        return True
+    else:
+        return False
+    
